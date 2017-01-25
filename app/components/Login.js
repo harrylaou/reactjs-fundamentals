631f6845baa290e65bec3d583d437488f51c2d8f
Marc Rivelles <rivelles.marc@gmail.com>
2017-01-25 13:57:09 +0000

    Solution 007

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,6 +2,29 @@ import React, {Component} from 'react'
 import {FormGroup, FormControl, Button} from 'react-bootstrap'
 
 class Login extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (name, event) {
+    let change = {}
+    change[name] = event.target.value
+    this.setState(change)
+  }
+
+  handleSubmit () {
+    if (this.state.username === 'reactboy' &&
+        this.state.password === '1234') {
+
+       this.context.router.push('/')
+    }
+  }
+
   render () {
     return (
       <form className="form-signin">
@@ -10,14 +33,39 @@ class Login extends Component {
         </FormGroup>
 
         <FormGroup>
-          <FormControl className="form-control" id="email" type="email" value="" placeholder="Enter email" />
-          <FormControl className="form-control" id="password" type="password" value="" placeholder="Password" />
+          <FormControl
+            className="form-control"
+            id="email"
+            type="email"
+            value={this.state.username}
+            onChange={this.handleChange.bind(this, 'username')}
+            placeholder="Enter email"
+          />
+          <FormControl
+            className="form-control"
+            id="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange.bind(this, 'password')}
+            placeholder="Password"
+          />
         </FormGroup>
 
-        <Button bsSize="large" bsStyle="primary" block type="submit">Sign in</Button>
+        <Button
+          bsSize="large"
+          bsStyle="primary"
+          block type="submit"
+          onClick={this.handleSubmit}
+        >
+          Sign in
+        </Button>
       </form>
     )
   }
+}
+
+Login.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
 
 export default Login
